49345482139b1d586b40a93c4ac4787219a726f9
Fantamomo <git@fantamomo.com>
2026-02-26 18:26:51 +0100

    chore: bump version to 1.1-SNAPSHOT and downgrade JVM toolchain to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.fantamomo.mc"
-version = "1.0-SNAPSHOT"
+version = "1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -23,7 +23,7 @@ paperweight.reobfArtifactConfiguration =
     io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 kotlin {
-    jvmToolchain(24)
+    jvmToolchain(21)
 }
 
 tasks.test {
