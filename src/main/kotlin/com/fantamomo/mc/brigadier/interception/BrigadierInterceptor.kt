c837099452c26667eb7a91c8d29d8a806c62a807
Fantamomo <git@fantamomo.com>
2026-06-10 22:58:11 +0200

    feat: enhance BrigadierInterceptor with path resolution and exception handling

--- src/main/kotlin/com/fantamomo/mc/brigadier/interception/BrigadierInterceptor.kt
+++ src/main/kotlin/com/fantamomo/mc/brigadier/interception/BrigadierInterceptor.kt
@@ -1,23 +1,53 @@
 package com.fantamomo.mc.brigadier.interception
 
+import com.fantamomo.mc.brigadier.interception.errors.UnknownPathSegmentException
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.tree.CommandNode
 import io.papermc.paper.command.brigadier.CommandSourceStack
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
+import io.papermc.paper.command.brigadier.ShadowBrigNode
 
-object BrigadierInterceptor {
+class BrigadierInterceptor(private val dispatcher: CommandDispatcher<CommandSourceStack>) {
 
-    fun builder(
-        dispatcher: CommandDispatcher<CommandSourceStack>
-    ) = InterceptionBuilder(dispatcher)
+    @Suppress("UNCHECKED_CAST")
+    fun path(path: List<String>): InceptionTarget {
+        var current: CommandNode<CommandSourceStack> = dispatcher.root
 
-    @OptIn(ExperimentalContracts::class)
-    inline fun build(
-        dispatcher: CommandDispatcher<CommandSourceStack>,
-        block: InterceptionBuilder.() -> Unit
-    ) {
-        contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
-        builder(dispatcher).apply(block).install()
+        for ((index, segment) in path.withIndex()) {
+            current = current
+                .getChild(segment)
+                ?.unbox()
+                    as? CommandNode<CommandSourceStack>
+                ?: throw UnknownPathSegmentException(
+                    "Could not find path segment '$segment'($index) in $path"
+                )
+        }
+
+        return InceptionTarget(current)
     }
+
+    fun path(vararg path: String): InceptionTarget = path(path.toList())
+
+//    fun byCommand(command: String): InceptionTarget {
+//        val server = MinecraftServer.getServer()
+//        val results = dispatcher.parse(
+//            command, net.minecraft.commands.CommandSourceStack(
+//                CommandSource.NULL,
+//                Vec3.ZERO,
+//                Vec2.ZERO,
+//                server.allLevels.first(),
+//                PermissionSet.ALL_PERMISSIONS,
+//                "",
+//                Component.empty(),
+//                server,
+//                null
+//            )
+//        )
+//        results.context
+//    }
+
+    private fun CommandNode<out CommandSourceStack>.unbox(): CommandNode<out CommandSourceStack> =
+        when (this) {
+            is ShadowBrigNode -> this.handle // ShadowBrigNode is a wrapper used by Paper to prevent developers from accessing the original node, which we ignore
+            else -> this
+        }
 }
